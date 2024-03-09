4f4b9e3319219a511bab8e1788250e8756b51056
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2024-03-09 13:14:26 +0200

    Add exam task

---
+++ blog/src/main/java/org/example/services/AccountService.java
@@ -0,0 +1,58 @@
+package org.example.services;
+
+import lombok.RequiredArgsConstructor;
+import org.example.configuration.security.JwtService;
+import org.example.dto.account.AuthResponseDto;
+import org.example.dto.account.LoginDto;
+import org.example.dto.account.RegisterDto;
+import org.example.entities.UserEntity;
+import org.example.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthResponseDto login(LoginDto request) {
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var isValid = passwordEncoder.matches(request.getPassword(), user.getPassword());
+        if(!isValid) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        var jwtToekn = jwtService.generateAccessToken(user);
+        return AuthResponseDto.builder()
+                .token(jwtToekn)
+                .build();
+    }
+
+    public AuthResponseDto register(RegisterDto request) {
+        // Перевірка існуючого користувача
+        Optional<UserEntity> existingUser = userRepository.findByEmail(request.getEmail());
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("User with this email already exists");
+        }
+
+        UserEntity newUser = UserEntity.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .phone(request.getPhone())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        UserEntity savedUser = userRepository.save(newUser);
+
+        var jwtToekn = jwtService.generateAccessToken(newUser);
+        return AuthResponseDto.builder()
+                .token(jwtToekn)
+                .build();
+    }
+}
